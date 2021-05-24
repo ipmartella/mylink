22a0aaf752b8fa1d19c7a23f2592789e6fcbadbc
Ippazio Martella <ippazio.martella@gmail.com>
2021-05-25 00:14:17 +0200

    Test case on add redone using BDD style test

--- tests/test_add.cpp
+++ tests/test_add.cpp
@@ -22,17 +22,21 @@ struct TestCollection {
 };
 
 
-TEST_CASE("Add a link, to a empty collection") {
-    //Given: an empty collection
-    TestCollection test_collection;
+SCENARIO("Add bookmark to collection") {
 
-    //When: I add a new bookmark
-    Bookmark new_bookmark{"http://www.wikipedia.org"};
-    test_collection.collection.add(new_bookmark);
+    GIVEN("An empty collection") {
+        TestCollection test_collection;
 
-    //Then: The collection contains the link
-    CHECK_EQ(test_collection.size(), 1);
-    CHECK(test_collection.containsBookmark(new_bookmark));
+        WHEN("I add a bookmark") {
+            Bookmark new_bookmark{"http://www.wikipedia.org"};
+            test_collection.collection.add(new_bookmark);
+
+            THEN("The collection contains only the new bookmark") {
+                CHECK_EQ(test_collection.size(), 1);
+                CHECK(test_collection.containsBookmark(new_bookmark));
+            }
+        }
+    }
 }
 
 TEST_CASE("Add a new link, to an existing collection") {

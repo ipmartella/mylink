0c9c43f5e214bb37a7333ac2dd7efde6bd6559cd
Ippazio Martella <ippazio.martella@gmail.com>
2021-05-25 22:17:36 +0200

    Refactor of markdown_storage.cpp

--- lib/src/markdown_storage.cpp
+++ lib/src/markdown_storage.cpp
@@ -8,14 +8,27 @@ using namespace mylink;
 
 namespace {
 
+class no_match_found : public std::domain_error {
+public:
+    no_match_found(const std::regex& regex, const std::string& target)
+        : std::domain_error{"Regexp does not match"}, regex_{regex}, target_{target} {}
+
+    const std::regex& regex() { return regex_; }
+    const std::string& target() { return target_; }
+
+private:
+    std::regex regex_;
+    std::string target_;
+};
+
 const std::regex regexp_simple(R"(^\s*[\*-]\s*(\S+)\s*$)");
 
-Bookmark matches(const std::string& line) {
+Bookmark convert_plain_url_list_item_to_bookmark(const std::string& line) {
     std::smatch match;
     if(std::regex_match(line, match, regexp_simple)) {
         return Bookmark{match[1]};
     } else {
-        throw std::out_of_range{"No match"};
+        throw no_match_found(regexp_simple, line);
     }
 }
 
@@ -23,8 +36,8 @@ Bookmark matches(const std::string& line) {
 
 Bookmark mylink::parse_markdown_line(const std::string& line) {
     try {
-        return matches(line);
-    }  catch (std::out_of_range) {
+        return convert_plain_url_list_item_to_bookmark(line);
+    }  catch (no_match_found) {
         return INVALID_BOOKMARK;
     }
 }

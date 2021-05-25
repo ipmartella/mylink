9fa8c596c5f8f8c633bc08f667060faffccca835
Ippazio Martella <ippazio.martella@gmail.com>
2021-05-25 22:03:10 +0200

    Parsing of simple markdown line starting with dash followed by a url works Example: - http://www.wikipedia.org

--- lib/src/markdown_storage.cpp
+++ lib/src/markdown_storage.cpp
@@ -1,13 +1,30 @@
 #include <markdown_storage.h>
 
 #include <string>
+#include <regex>
+#include <stdexcept>
 
 using namespace mylink;
 
 namespace {
 
-bool is_white_space_only(const std::string& line) {}
+const std::regex regexp_simple(R"(^\s*-\s*(\S+)\s*$)");
+
+Bookmark matches(const std::string& line) {
+    std::smatch match;
+    if(std::regex_match(line, match, regexp_simple)) {
+        return Bookmark{match[1]};
+    } else {
+        throw std::out_of_range{"No match"};
+    }
+}
 
 } //namespace
 
-Bookmark parse_markdown_line(const std::string& line) {}
+Bookmark mylink::parse_markdown_line(const std::string& line) {
+    try {
+        return matches(line);
+    }  catch (std::out_of_range) {
+        return INVALID_BOOKMARK;
+    }
+}
--- tests/test_markdown_format.cpp
+++ tests/test_markdown_format.cpp
@@ -15,6 +15,18 @@ void line_is_parsed_but_no_result(const std::string& line)
     }
 }
 
+void line_is_parsed_with_url(const std::string& line, const std::string& expected_url) {
+    WHEN("The Markdown line is parsed")
+    {
+        INFO("Line:<", line, ">");
+        auto bookmark = parse_markdown_line(line);
+
+        THEN("A bookmark will be created") {
+            CHECK_EQ(bookmark.url, expected_url);
+        }
+    }
+}
+
 SCENARIO("Covert Markdown to bookmark")
 {
     GIVEN("A empty line")
@@ -25,11 +37,32 @@ SCENARIO("Covert Markdown to bookmark")
         }
     }
 
-    GIVEN("- https://www.wikipedia.org")
+    GIVEN("Variations of <- https://www.wikipedia.org>")
     {
-        WHEN("The Markdown line is parsed")
-        {
-            THEN("A bookmark will be created") {}
+        std::vector<std::string> empty_lines = {
+            "- https://www.wikipedia.org",
+            " - https://www.wikipedia.org  ",
+            "\t-https://www.wikipedia.org",
+            "-https://www.wikipedia.org\r\n",
+            "-https://www.wikipedia.org \n",
+        };
+
+        for (const auto& line : empty_lines) {
+            line_is_parsed_with_url(line, "https://www.wikipedia.org");
+        }
+    }
+    GIVEN("Variations of <* https://www.wikipedia.org>")
+    {
+        std::vector<std::string> empty_lines = {
+            "* https://www.wikipedia.org",
+            " * https://www.wikipedia.org  ",
+            "\t*https://www.wikipedia.org",
+            "*https://www.wikipedia.org\r\n",
+            "*https://www.wikipedia.org \n",
+        };
+
+        for (const auto& line : empty_lines) {
+            line_is_parsed_with_url(line, "https://www.wikipedia.org");
         }
     }
 }

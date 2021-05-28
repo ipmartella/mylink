68085b10eeebe3c1f9a3d6166d5a7b785b2757fb
Ippazio Martella <ippazio.martella@gmail.com>
2021-05-28 23:57:58 +0200

    Markdown export to stream

--- lib/src/markdown/markdown_export.cpp
+++ lib/src/markdown/markdown_export.cpp
@@ -9,14 +9,20 @@ std::string mylink::convert_to_markdown_line(const Bookmark& bookmark) {
     return markdown_line_builder.str();
 }
 
+void mylink::write_bookmarks_to_stream(const std::vector<Bookmark>& bookmarks, std::ostream& stream) {
+    for(const auto& bookmark : bookmarks) {
+        stream << convert_to_markdown_line(bookmark) << '\n';
+    }
+}
+
+
 #ifdef MYLINK_TEST_IN_CODE
 #include <doctest.h>
+#include <sstream>
 
 using namespace mylink;
 
 SCENARIO("Convert Bookmark to Markdown line") {
-    MESSAGE("Hi there!");
-
     GIVEN("A bookmark with a plain URL") {
         Bookmark plain_url_bookmark("http://www.wikipedia.org");
 
@@ -24,6 +30,40 @@ SCENARIO("Convert Bookmark to Markdown line") {
             std::string markdown_line = convert_to_markdown_line(plain_url_bookmark);
             THEN("The line is like: - http://www.url.org") {
                 CHECK_EQ(markdown_line, "- http://www.wikipedia.org");
+            }
+        }
+    }
+}
+
+SCENARIO("Convert vector of Bookmarks to Markdown stream") {
+    GIVEN("A empty vector of bookmarks") {
+        std::vector<Bookmark> bookmarks;
+        WHEN("The bookmarks are written to a stream") {
+            std::stringstream stream;
+            write_bookmarks_to_stream(bookmarks, stream);
+
+            THEN("The stream is empty") {
+                CHECK(stream.str().empty());
+            }
+        }
+    }
+
+    GIVEN("A vector of bookmarks") {
+        std::vector<Bookmark> bookmarks{
+            Bookmark{"http://www.wikipedia.org"},
+            Bookmark{"http://www.url.org"},
+            Bookmark{"http://www.mytesturl.org/test/index"},
+        };
+        WHEN("The bookmarks are written to a stream") {
+            std::stringstream stream;
+            write_bookmarks_to_stream(bookmarks, stream);
+
+            THEN("The stream contains the URL in Markdown format") {
+                CHECK_EQ(stream.str(),
+                         "- http://www.wikipedia.org\n"
+                         "- http://www.url.org\n"
+                         "- http://www.mytesturl.org/test/index\n");
+
             }
         }
     }

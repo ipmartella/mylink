d5149f2fbbf991ed60d167d59a3713df5813338e
Ippazio Martella <ippazio.martella@gmail.com>
2021-06-13 17:35:24 +0200

    MarkdownCollection.add tests moved to a separate file

--- lib/src/markdown/markdown_collection.cpp
+++ lib/src/markdown/markdown_collection.cpp
@@ -15,80 +15,15 @@ bool bookmark_in_collection(const Bookmark& target, const std::vector<Bookmark>&
     return false;
 }
 
-void add_bookmark_to_file(const Bookmark& bookmark, const std::string& filename) {
-    std::vector<Bookmark> bookmarks = read_bookmarks_from_file(filename);
-    if (!bookmark_in_collection(bookmark, bookmarks)) {
-        bookmarks.push_back(bookmark);
-        write_bookmarks_to_file(bookmarks, filename);
-    }
-}
-
-
 } //namespace
 
 MarkdownCollection::MarkdownCollection(const std::string& markdown_file_name) : filename_{markdown_file_name} {}
 
 void MarkdownCollection::add(const Bookmark& bookmark)
 {
-    add_bookmark_to_file(bookmark, filename_);
-}
-
-#ifdef MYLINK_TEST_IN_CODE
-#include <doctest.h>
-
-SCENARIO("Adding bookmarks to files") {
-
-    GIVEN("A empty file") {
-        const std::string test_file = "/tmp/mylink_test1";
-        std::remove(test_file.c_str());
-
-        WHEN("I add a bookmark in Markdown format") {
-            Bookmark new_bookmark{"http://www.wikipedia.org"};
-            add_bookmark_to_file(new_bookmark, test_file);
-
-            THEN("The stream contains the bookmark") {
-                auto saved_bookmarks = read_bookmarks_from_file(test_file);
-                CHECK_EQ(saved_bookmarks.size(), 1);
-                CHECK_EQ(saved_bookmarks[0].get_url(), "http://www.wikipedia.org");
-            }
-        }
+    std::vector<Bookmark> bookmarks = read_bookmarks_from_file(filename_);
+    if (!bookmark_in_collection(bookmark, bookmarks)) {
+        bookmarks.push_back(bookmark);
+        write_bookmarks_to_file(bookmarks, filename_);
     }
-
-    GIVEN("A populated stream") {
-        const std::string test_file = "/tmp/mylink_test2";
-        std::remove(test_file.c_str());
-        std::vector<Bookmark> initial_bookmarks{
-            Bookmark("http://www.wikipedia.org"),
-            Bookmark("http://www.myurl.org"),
-        };
-        write_bookmarks_to_file(initial_bookmarks, test_file);
-
-        WHEN("I add a new bookmark in Markdown format") {
-            Bookmark new_bookmark{"http://www.test.org"};
-            add_bookmark_to_file(new_bookmark, test_file);
-
-            THEN("The stream contains the bookmark + all the previous ones") {
-                auto saved_bookmarks = read_bookmarks_from_file(test_file);
-                CHECK_EQ(saved_bookmarks.size(), 3);
-                CHECK(saved_bookmarks[0].is_url_same_as(initial_bookmarks[0]));
-                CHECK(saved_bookmarks[1].is_url_same_as(initial_bookmarks[1]));
-                CHECK(saved_bookmarks[2].is_url_same_as(new_bookmark));
-            }
-        }
-
-        WHEN("I add an existing bookmark in Markdown format") {
-            add_bookmark_to_file(initial_bookmarks[0], test_file);
-            add_bookmark_to_file(initial_bookmarks[1], test_file);
-
-            THEN("The stream contains only the initial bookmarks") {
-                auto saved_bookmarks = read_bookmarks_from_file(test_file);
-                CHECK_EQ(saved_bookmarks.size(), 2);
-                CHECK(saved_bookmarks[0].is_url_same_as(initial_bookmarks[0]));
-                CHECK(saved_bookmarks[1].is_url_same_as(initial_bookmarks[1]));
-            }
-        }
-    }
-
 }
-
-#endif

4c18a1eb298d1c7611af281932c483fd44d8ce60
Ippazio Martella <ippazio.martella@gmail.com>
2021-06-24 00:12:15 +0200

    Removed useless files

--- lib/src/virtual_collection.cpp
+++
@@ -1,9 +0,0 @@
-#include <virtual_collection.h>
-
-using namespace mylink;
-
-VirtualCollection::VirtualCollection() : bookmarks_{} {}
-
-void VirtualCollection::add(const Bookmark &bookmark) {
-    bookmarks_.push_back(bookmark);
-}

c82df9631db0cd9a50fbc60d9e492e39335db966
Ippazio Martella <ippazio.martella@gmail.com>
2021-07-01 22:00:16 +0200

    Added bookmarklet link in HTML page

--- server/bookmarklet.js
+++ server/bookmarklet.js
@@ -1,1 +1,25 @@
-javascript:(function() { var request = { url: window.location.href, title: document.title }; window.fetch("http://localhost:1234/add_bookmark", { method: 'POST', headers: { 'Content-Type': 'application/json' }, body: JSON.stringify(request) }).then(risposta => { if (!risposta.ok) { console.log("Fallito"); } }); })();
+//Go to https://mrcoles.com/bookmarklet to convert this into a bookmarklet
+var request = {
+  url: window.location.href,
+  title: document.title
+};
+
+window.fetch("http://localhost:1234/bookmarks", {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(request)
+  })
+  .then(response => {
+    if (response.status === 201) {
+      alert("Added to mylinks!");
+    } else if (response.status === 409) {
+      alert("Link already exists in mylinks");
+    } else {
+      alert("Couldn't add link to mylinks :(. Error: " + response.status);
+    }
+  })
+  .catch(err => {
+    alert("Couldn't add link to mylinks :(. Are you sure mylinkserver is running?");
+  });

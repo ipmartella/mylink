eb007c738b265dde8879e1c18fd93ca94fbc72f8
Ippazio Martella <ippazio.martella@gmail.com>
2021-07-19 21:03:28 +0200

    Added exploratory tests for cpp-httplib

---
+++ ext/cpp-httplib/test_cpp-httplib.cpp
@@ -0,0 +1,19 @@
+#include <doctest.h>
+#include "httplib.h"
+
+SCENARIO("Tests with file mounting") {
+    GIVEN("A cpp-httplib server") {
+        httplib::Server server;
+        WHEN("I try to mount a folder that exists") {
+            THEN("I get true as a return value") {
+                CHECK(server.set_mount_point("/", "/opt"));
+            }
+        }
+
+        WHEN("I try to mount a folder that does not exist") {
+            THEN("I get false as a return value") {
+                CHECK_FALSE(server.set_mount_point("/", "/non_existing_path"));
+            }
+        }
+    }
+}
